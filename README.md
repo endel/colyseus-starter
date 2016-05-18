5717ad165d83d0ba1e2d0789c043a09e19860648
Endel Dreyer <endel.dreyer@gmail.com>
2016-05-18 17:30:43 +0100

    updates for colyseus v0.4.0

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 colyseus-starter
 ===
 
-Simplest project structure for running Colyseus Game Server. [View docs](http://gamestd.io/colyseus/docs)
+Barebones project structure for running Colyseus Game Server. [View docs](http://gamestd.io/colyseus/docs)
 
 Dependencies
 ---
 
 - [colyseus](http://npmjs.com/package/colyseus)
 - [express](http://npmjs.com/package/express)
-- [clock-timer.js](http://npmjs.com/package/clock-timer.js) (optional)
 
 **Development dependencies**
 
--- rooms/chat_room.js
+++ rooms/chat_room.js
@@ -3,8 +3,17 @@ var Room = require('colyseus').Room
 class ChatRoom extends Room {
 
   constructor (options) {
-    // call 'update' method each 50ms
-    super(options, 1000 / 20)
+    super( options )
+
+    // Broadcast patched state to all connected clients at 20fps (50ms)
+    this.setPatchRate( 1000 / 20 )
+
+    // // Call this function if you intend to implement delay compensation
+    // // techniques in your game
+    // this.useTimeline()
+
+    // Call game simulation at 60fps (16.6ms)
+    this.setSimulationInterval( this.tick.bind(this), 1000 / 60 )
 
     this.setState({ messages: [] })
   }
@@ -24,10 +33,29 @@ class ChatRoom extends Room {
     this.state.messages.push(`${ client.id }: ${ data }`)
   }
 
+  tick () {
+    //
+    // This is your 'game loop'.
+    // Inside function you'll have to run the simulation of your game.
+    //
+    // You should:
+    // - move entities
+    // - check for collisions
+    // - update the state
+    //
+
+    // // Uncomment this line to see the simulation running and clients receiving the patched state
+    // // In this example, the server simply adds the elapsedTime every 2 messages it receives
+    // if ( this.state.messages.length % 3 == 0 ) {
+    //   this.state.messages.push(`${ this.clock.elapsedTime }: even`)
+    // }
+  }
+
   onLeave (client) {
     console.log(client.id, "leaved ChatRoom")
     this.state.messages.push(`${ client.id } leaved.`)
   }
+
 }
 
 module.exports = ChatRoom
